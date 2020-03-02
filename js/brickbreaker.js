9699653679939d9412e6dec6f0547fec844f1197
Sarah Rosales <srosales@ucvts.org>
2020-03-01 19:18:40 -0500

    moving ball

--- js/brickbreaker.js
+++ js/brickbreaker.js
@@ -3,16 +3,18 @@
 ///////////////////// APP STATE (VARIABLES) /////////////////////////
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
-var x = canvas.width/2;
-var y = canvas.height-30;
+var ballRadius = 11;
+var x = canvas.width / 2;
+var y = canvas.height - 30;
 var dx = 2;
 var dy = -2;
+
 ///////////////////// CACHED ELEMENT REFERENCES /////////////////////
 ///////////////////// EVENT LISTENERS ///////////////////////////////
 ///////////////////// FUNCTIONS /////////////////////////////////////
 function drawBall() {
     ctx.beginPath();
-    ctx.arc(x, y, 10, 0, Math.PI*2);
+    ctx.arc(x, y, ballRadius, 0, Math.PI*2);
     ctx.fillStyle = "#0095DD";
     ctx.fill();
     ctx.closePath();
@@ -20,13 +22,17 @@ function drawBall() {
 
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.beginPath();
-  ctx.arc(x, y, 10, 0, Math.PI*2);
-  ctx.fillStyle = "#0095DD";
-  ctx.fill();
-  ctx.closePath();
-  x += dx;
-  y += dy;
+  drawBall();
+
+  if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
+         dx = -dx;
+     }
+     if(y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
+         dy = -dy;
+     }
+
+     x += dx;
+     y += dy;
 }
 
 setInterval(draw, 10);
