19070f2551664c73b3a4f536070bff0949d653c1
Sarah Rosales <srosales@ucvts.org>
2020-03-01 19:10:31 -0500

    circles

---
+++ js/brickbreaker.js
@@ -0,0 +1,32 @@
+///////////////////// CONSTANTS /////////////////////////////////////
+
+///////////////////// APP STATE (VARIABLES) /////////////////////////
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+var x = canvas.width/2;
+var y = canvas.height-30;
+var dx = 2;
+var dy = -2;
+///////////////////// CACHED ELEMENT REFERENCES /////////////////////
+///////////////////// EVENT LISTENERS ///////////////////////////////
+///////////////////// FUNCTIONS /////////////////////////////////////
+function drawBall() {
+    ctx.beginPath();
+    ctx.arc(x, y, 10, 0, Math.PI*2);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+}
+
+function draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.beginPath();
+  ctx.arc(x, y, 10, 0, Math.PI*2);
+  ctx.fillStyle = "#0095DD";
+  ctx.fill();
+  ctx.closePath();
+  x += dx;
+  y += dy;
+}
+
+setInterval(draw, 10);
